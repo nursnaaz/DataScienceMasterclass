b3be90361fb5339ae3ef56c112cdcc3109742a71
Mohamed Noordeen Alaudeen <mohamednoordeenalaudeen@192.168.1.8>
2023-05-14 02:36:09 +0530

    Python

---
+++ Python/Week 02 - Python Basics/operation.py
@@ -0,0 +1,11 @@
+def find_sum(a,b):
+    print("Addition of a and b = ", a+b)
+    return a+b
+
+def find_sub(a,b):
+    print("Subtraction of a and b = ", a-b)
+    return a-b
+
+def find_mul(a,b):
+    print("Multiplication of a and b = ", a*b)
+    return a*b
